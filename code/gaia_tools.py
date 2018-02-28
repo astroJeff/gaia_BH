a1ae908c8a45d62877b72f7bf15569bc62f7e231
katiebreivik <katelynbreivik2013@u.northwestern.edu>
2018-02-28 10:28:49 -0600

    added pyGaia functionality for parallax and position errors, implemented test in run_set_pygaia.py; not yet tested though

--- code/gaia_tools.py
+++ code/gaia_tools.py
@@ -1,11 +1,17 @@
 import numpy as np
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from scipy.interpolate import interp1d
+from pygaia.errors.astrometric import positionError as posErr
+from pygaia.errors.astrometric import parallaxError as plxErr
+from pygaia.errors.utils import averageNumberOfTransits as N_transit_ave
 
 
-def get_single_obs_err(G=None, V=None, V_IC=None):
+def get_single_obs_plx_err(G=None, V=None, V_IC=None, RA=None, Dec=None):
     """ A tool to determine the single observation astrometric precision. This is
     calculated in a bit of an ad hoc way. We call the overall parallax precision, then
-    multiply by sqrt(70), as this is the number that should have been used to determine
-    the parallax precision.
+    multiply by sqrt(N_obs), as this is the number that should have been used to determine
+    the parallax precision. NOTE: this uses the Gaia DPAC package: pyGaia
 
     Inputs
     ------
@@ -15,10 +21,16 @@ def get_single_obs_err(G=None, V=None, V_IC=None):
     V_IC : float
     Color index of the stellar type
 
+    RA : float
+    Right ascension in degrees
+
+    Dec: float
+    Declination in degrees
+
     Returns
     -------
-    sigma_plx : float
-    parallax uncertainty in arcseconds
+    plx_err_single : float
+    parallax uncertainty in arcseconds for a single observation
 
     """
 
@@ -30,21 +42,86 @@ def get_single_obs_err(G=None, V=None, V_IC=None):
     if G is None and V is None:
         print("You must provide at least one color: G or V")
         return
+    
+    if RA is None and Dec in None:
+        print("You must provide a RA and Dec")
+        return
 
+    # KMB: get ecliptic latitude from ra/dec using astropy
+    c = SkyCoord(ra=sys_ra*u.degree, dec=sys_dec*u.degree, frame='icrs')
+    ecl_lat = c.GeocentricTrueEcliptic.lat
 
-    plx_err = get_plx_err(G=G, V=V, V_IC=V_IC)
+    # KMB: compute average N_obs for given ecliptic latitude
+    N_obs_ave = N_transit_ave(ecl_lat)
+
+    plx_err = plxErr(G=G, vmini=V_IC, beta=ecl_lat)*1.0e-6
 
     m = 1.2
-    g_pi = 1.9
+    g_pi = 1.47/np.sin(ecl_lat)
 
-    pos_err = np.sqrt(70.0) / (m * g_pi) * plx_err
+    plx_err_single = np.sqrt(N_obs_ave) / (m * g_pi) * plx_err
 
-    return pos_err
-
-
+    return plx_err_single
 
 
-def get_plx_err(G=None, V=None, V_IC=None):
+
+def get_single_obs_pos_err(G=None, V=None, V_IC=None, RA=None, Dec=None):
+    """ A tool to determine the single observation astrometric precision. This is
+    calculated in a bit of an ad hoc way. We call the overall parallax precision, then
+    multiply by sqrt(N_obs), as this is the number that should have been used to determine
+    the parallax precision. NOTE: this uses the Gaia DPAC package: pyGaia
+
+    Inputs
+    ------
+    G or V : float (provide one of these)
+    Gaia G-band or Visual V-band magnitude of the star
+
+    V_IC : float
+    Color index of the stellar type
+
+    RA : float
+    Right ascension in degrees
+
+    Dec: float
+    Declination in degrees    
+     
+    Returns
+    -------
+    pos_err_single : float
+    position uncertainty in arcseconds for a single observation
+
+    """
+
+    if V_IC is None:
+        print("You must provide a color index in the form of V_IC.")
+        return
+
+    if G is None and V is None:
+        print("You must provide at least one color: G or V")
+        return
+
+    if RA is None and Dec in None:
+        print("You must provide a RA and Dec")
+        return
+
+    # KMB: get ecliptic latitude from ra/dec using astropy
+    c = SkyCoord(ra=sys_ra*u.degree, dec=sys_dec*u.degree, frame='icrs')
+    ecl_lat = c.GeocentricTrueEcliptic.lat
+
+    # KMB: compute average N_obs for given ecliptic latitude
+    N_obs_ave = N_transit_ave(ecl_lat)
+
+    pos_err = posErr(G=G, vmini=V_IC, beta=ecl_lat)*1.0e-6
+
+    m = 1.2
+    g_pi = 1.47/np.sin(ecl_lat)
+
+    pos_err_single = np.sqrt(N_obs_ave) / (m * g_pi) * pos_err
+
+    return pos_err_single
+
+
+def get_plx_err(G=None, V=None, V_IC=None, RA=None, Dec=None):
     """ A tool to determine the parallax uncertainty for a star
 
     Inputs
@@ -55,9 +132,15 @@ def get_plx_err(G=None, V=None, V_IC=None):
     V_IC : float
     Color index of the stellar type
 
+    RA : float
+    Right ascension in degrees
+
+    Dec: float
+    Declination in degrees
+
     Returns
     -------
-    sigma_plx : float
+    plx_err : float
     parallax uncertainty in arcseconds
 
     """
@@ -73,14 +156,62 @@ def get_plx_err(G=None, V=None, V_IC=None):
 
     if G is None:  G = V - 0.0257 - 0.0924 * (V_IC) - 0.1623 * (V_IC)**2 + 0.0090 * (V_IC)**3
 
-    z = np.max([10**(0.4 * (12.09 - 15.0)), 10**(0.4 * (G - 15.0))])
-    sigma_plx = np.sqrt(-1.631 + 680.766*z + 32.732*z**2) * (0.986 + (1 - 0.986) * V_IC)
+    # KMB: get ecliptic latitude from ra/dec using astropy
+    c = SkyCoord(ra=sys_ra*u.degree, dec=sys_dec*u.degree, frame='icrs')
+    ecl_lat = c.GeocentricTrueEcliptic.lat
 
-    # Convert from microas to asec
-    sigma_plx = sigma_plx/1.0e6
+    # KMB: compute average N_obs for given ecliptic latitude
+    N_obs_ave = N_transit_ave(ecl_lat)
 
-    return sigma_plx
+    plx_err = plxErr(G=G, vmini=V_IC, beta=ecl_lat)*1.0e-6
 
+    return plx_err
+
+
+def get_pos_err(G=None, V=None, V_IC=None, RA=None, Dec=None):
+    """ A tool to determine the parallax uncertainty for a star
+
+    Inputs
+    ------
+    G or V : float (provide one of these)
+    Gaia G-band or Visual V-band magnitude of the star
+
+    V_IC : float
+    Color index of the stellar type
+
+    RA : float
+    Right ascension in degrees
+
+    Dec: float
+    Declination in degrees
+
+    Returns
+    -------
+    pos_err : float
+    position uncertainty in arcseconds
+
+    """
+
+    if V_IC is None:
+        print("You must provide a color index in the form of V_IC.")
+        return
+
+    if G is None and V is None:
+        print("You must provide at least one color: G or V")
+        return
+
+    if G is None:  G = V - 0.0257 - 0.0924 * (V_IC) - 0.1623 * (V_IC)**2 + 0.0090 * (V_IC)**3
+
+    # KMB: get ecliptic latitude from ra/dec using astropy
+    c = SkyCoord(ra=sys_ra*u.degree, dec=sys_dec*u.degree, frame='icrs')
+    ecl_lat = c.GeocentricTrueEcliptic.lat
+
+    # KMB: compute average N_obs for given ecliptic latitude
+    N_obs_ave = N_transit_ave(ecl_lat)
+
+    pos_err = posErr(G=G, vmini=V_IC, beta=ecl_lat)*1.0e-6
+
+    return pos_err
 
 
 def calc_new_magnitude(mag_current, distance_current, distance_new):
