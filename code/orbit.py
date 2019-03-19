d8c9118d6a378ca997eec2ae02dfb84216b72c23
AstroJeff <andrews@astro.columbia.edu>
2019-03-19 16:22:04 +0100

    Added doc strings to all functions

--- code/orbit.py
+++ code/orbit.py
@@ -24,34 +24,54 @@ def a_to_P(M1, M2, a):
     n = np.sqrt(mu/(a**3 * AUincm**3))
     return 2.0*np.pi / n
 
-# Function to get the true anomaly
 def get_f(p, t_set):
+    """
+    Calculate the true anomaly
 
+    Arguments
+    ---------
+    p : tuple
+        Set of model parameters
+    t_set : float, numpy array
+        Observations times for true anomaly calculation
+
+    Returns
+    -------
+    f_set : float, numpy array
+        Set of true anomalies
+    """
+
+    # Load model parameters
     sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance = p
 
+    # Calculate orbital separation
     mu = G * (M1 + M2)
     n = 2.0*np.pi / P
     A = np.power(mu/(n*n), 1.0/3.0)
 
-
+    # Kepler's equation
     def func_E(x,n,t,tau,e):
         return n*(t-tau) - x + e*np.sin(x)
 
+    # Iterate through t_set, calculate eccentric anomaly - requires a root finder, so cannot be vectorized
     E_set = np.array([])
     if isinstance(t_set, np.ndarray):
         for t in t_set:
             try:
+                # Use Newton root-finder to solve Kepler's equation
                 E_new = newton(func_E, t, args=(n,t,tau,e))
             except:
                 return None
             E_set = np.append(E_set, E_new)
     else:
         try:
+            # Use Newton root-finder to solve Kepler's equation
             E_new = newton(func_E, t_set, args=(n,t_set,tau,e))
         except:
             return None
         E_set = np.append(E_set, E_new)
 
+    # For each eccentric anomaly, calculate the true anomaly
     f_set = np.array([])
     for E in E_set:
         f = np.arccos((np.cos(E)-e)/(1.0-e*np.cos(E)))
@@ -60,64 +80,114 @@ def get_f(p, t_set):
 
         f_set = np.append(f_set, f)
 
-
     return f_set
 
 
-# Function to get the r component of the velocity
-def get_r_dot(p, t):
 
+def get_r_dot(p, t):
+    """
+    Function to get the r component of the velocity
+
+    Arguments
+    ---------
+    p : tuple
+        Set of model parameters
+    t : float, numpy array
+        Observation times
+
+    Returns
+    -------
+    r_dot : float, numpy array
+        Radial velocities
+    """
+
+    # Load up model parameters
     sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance = p
 
+    # Calculate the orbital separation
     mu = G * (M1 + M2)
     n = 2.0*np.pi / P
     A = np.power(mu/(n*n), 1.0/3.0)
+
+    # Calculate true anomalies
     f = get_f(p, t)
     if f is None: return None
 
+    # Calculate radial velocities
     r_dot = A * e * np.sin(f) / np.sqrt(1.0 - e*e) * n
 
     return r_dot
 
 
-# Function to get the phi component of the velocity (?)
 def get_r_f_dot(p, t):
+    """
+    Calculate the phi component of the velocity
 
+    Arguments
+    ---------
+    p : tuple
+        Set of model parameters
+    t : float, numpy array
+        Observation times
+
+    Returns
+    -------
+    r_f_dot : float, numpy array
+        Azimuthal velocities
+    """
+
+    # Load model parameters
     sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance = p
 
+    # Calculate the orbital separation
     mu = G * (M1 + M2)
     n = 2.0*np.pi / P
     A = np.power(mu/(n*n), 1.0/3.0)
+
+    # Calculate true anomalies
     f = get_f(p, t)
     if f is None: return None
 
+    # Calculate the azimuthal velocities
     r_f_dot = n * A / np.sqrt(1.0 - e*e) * (1.0 + e*np.cos(f))
 
     return r_f_dot
 
-# Radial velocity
-def get_RV(p, t):
 
+def get_RV(p, t):
+    """
+    Calculate the radial velocities
+
+    Arguments
+    ---------
+    p : tuple
+        Set of model parameters
+    t : float, numpy array
+        Observation times
+
+    Returns
+    -------
+    RV : float
+        Radial Velocities
+    """
+
+    # Load model parameters
     sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance = p
 
+    # Calculate A
     mu = G * (M1 + M2)
     n = 2.0*np.pi / P
     A = np.power(mu/(n*n), 1.0/3.0)
+
+    # Get true anomalies
     f = get_f(p, t)
     if f is None: return None
-
-    # r_dot = get_r_dot(p, t)
-    # r_f_dot = get_r_f_dot(p, t)
 
     # Get two RV components (these are in cm/s)
     rv = n * A / np.sqrt(1.0 - e*e) * (np.cos(omega+f) * np.sin(I) + e * np.cos(omega) * np.sin(I))
     rv = rv * M2 / (M1+M2)
 
-    # rv_1 = r_dot * M2 / (M1+M2) * (np.cos(Omega)*np.cos(omega+f) - np.sin(Omega)*np.sin(omega+f)*np.cos(I))
-    # rv_2 = r_f_dot * M2 / (M1+M2) * (np.cos(Omega)*np.sin(omega+f) + np.sin(Omega)*np.cos(omega+f)*np.cos(I))
-
-    # RV is in km/s
-    # rv = -(rv_1 - rv_2) / 1.0e5
+    # Convert from cm/s to km/s
     rv = rv / 1.0e5
 
     # Adjust for system's radial velocity
@@ -125,24 +195,45 @@ def get_RV(p, t):
 
     return rv
 
-# Function to get astrometry for a particular orbital solution and time
+
+
 
 def get_ra_dec(p, t):
+    """
+    Function to get astrometry for a particular orbital solution and time
+
+    Arguments
+    ---------
+    p : tuple
+        Set of model parameters
+    t : float, numpy array
+        Observation times
+
+    Returns
+    -------
+    ra, dec : output coordinates (deg)
+    """
+
+    # load model parameters
     sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance = p
 
+    # Calculate orbital separation
     mu = G * (M1 + M2)
     n = 2.0*np.pi / P
     A = np.power(mu/(n*n), 1.0/3.0)
+
+    # Calculate true anomalies
     f = get_f(p, t)
     if f is None: return None, None
 
     # Get the distance from M2 to the barycenter
     sep = A * (1.0 - e**2) / (1.0 + e * np.cos(f)) * (M2 / (M1+M2))
 
+    # Calculate the orbital motion
     d_alpha = sep * (np.cos(Omega)*np.cos(omega+f) - np.sin(Omega)*np.sin(omega+f)*np.cos(I)) / (distance*pcincm)
     d_delta = sep * (np.sin(Omega)*np.cos(omega+f) + np.cos(Omega)*np.sin(omega+f)*np.cos(I)) / (distance*pcincm)
 
-    # Work in arcseconds
+    # Convert to degrees, add in center of motion position
     alpha_out = sys_ra + d_alpha * (180.0/np.pi)
     delta_out = sys_dec + d_delta * (180.0/np.pi)
 
@@ -153,17 +244,38 @@ def get_ra_dec(p, t):
 
 
 def get_ra_dec_all(p, t, include_orbit=True, include_parallax=True, include_proper_motion=True):
+    """
+    Obtain position coordinates of a star in a binary including orbit, parallax, and proper motion
 
+    Arguments
+    ---------
+    p : tuple
+        Set of orbital parameters
+    t : float, numpy array
+        Observation times
+
+    Returns
+    -------
+    ra, dec : float, numpy array
+        Output positions (deg)
+    """
+
+    # Load up model parameters
     sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance, pm_ra, pm_dec = p
     p_orb = sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance
 
     # Get parallax from distance
     parallax = 1.0e3/distance
 
-    # Parallax
+    # Calculate the effect on position from parallax
     if include_parallax:
         sys_ra_hour = sys_ra / 360.0 * 24.0  # convert from decimal degrees to decimal hours
+
+        # Calculate parallax factors using parellipse from  J. Thorstensen's code SKYCALC
         output = parellipse(t, sys_ra_hour, sys_dec, 2015.0, 0.0, 0.0)
+
+        # Calculate actual effect on position from parallax by multiplying by parallax
+        # Since parallax is in units of mas, positions are currently also in mas
         ra_out = output[0] * parallax
         dec_out = output[1] * parallax
     else:
@@ -171,19 +283,20 @@ def get_ra_dec_all(p, t, include_orbit=True, include_parallax=True, include_prop
         dec_out = np.zeros(len(t))
 
 
-    # Proper Motion
+    # Add the effect from proper motion (also in mas)
     if include_proper_motion:
         ra_out += pm_ra * (t*secday - tau)/secday/365.25
         dec_out += pm_dec * (t*secday - tau)/secday/365.25
 
 
-    # Orbital Motion
+    # Calculate the effects of orbital motion
     if include_orbit:
         ra_orb, dec_orb = get_ra_dec(p_orb, t*secday)
         if ra_orb is None or dec_orb is None: return None, None
     else:
         ra_orb, dec_orb = 0.0, 0.0
 
+    # Combine the effects of parallax, proper motion, and orbital motion (deg)
     ra_out = ra_out/(3600.0*1.0e3) + ra_orb
     dec_out = dec_out/(3600.0*1.0e3) + dec_orb
 
--- code/run_grid.py
+++ code/run_grid.py
@@ -18,6 +18,20 @@ import photometry
 
 
 def load_t_obs(filename="../data/cadence/J1102.csv"):
+    """
+    Load up Gaia cadence data
+
+    Arguments
+    ---------
+    filename : string
+        File path and name for gaia cadence data
+
+    Returns
+    -------
+    data : numpy array
+        Gaia observation cadence data
+    """
+
     data = np.genfromtxt(filename, delimiter=',', names=True)
 
     return data
@@ -28,6 +42,39 @@ def load_t_obs(filename="../data/cadence/J1102.csv"):
 def start_position(M1, M2, distance, P_orb, t_obs, pm_ra, pm_dec,
                    sys_ra=165.5728333, sys_dec=41.2209444, Omega=np.pi/3.0,
                    omega=np.pi/3.0, I=np.pi/3.0, e=0.01, gamma=0.0):
+    """
+    Set the starting position for model parameters
+
+    Arguments
+    ---------
+    M1, M2 : float
+        Component masses (Msun)
+    distance : float
+        Distance to the binary (pc)
+    P_orb : float
+        Orbital period (days)
+    t_obs : float
+        Set of observation times
+    pm_ra, pm_dec : float
+        Proper motions (mas/yr)
+    sys_ra, sys_dec : float
+        Center of mass position coordinates
+    Omega : float
+        Longitude of the ascending node
+    omega : float
+        Argument of periapse
+    I : float
+        Inclination angle
+    e : float
+        Eccentricity
+    gamma : float
+        radial velocity zero point
+
+    Returns
+    -------
+    p : tuple
+        Set of model parameters
+    """
 
     # Observation times
     tau = np.min(t_obs) * c.secday
@@ -39,6 +86,20 @@ def start_position(M1, M2, distance, P_orb, t_obs, pm_ra, pm_dec,
 
 
 def run_one_M2(P_orb=100.0, M2=1.0e-3, nburn=1000, nrun=5000):
+    """
+    Run grid of models in distance and M1. Save model results.
+
+    Parameters
+    ----------
+    P_orb : float
+        Orbital period (days)
+    M2 : float
+        Companion mass (Msun)
+    nburn : int
+        Number of burn-in steps
+    nrun : int
+        Number of steps to run
+    """
 
     dist_set = np.logspace(1, 4, 4)
     M1_set = [1.0, 10.0]
@@ -57,8 +118,29 @@ def run_one_M2(P_orb=100.0, M2=1.0e-3, nburn=1000, nrun=5000):
 
 
 def run_one_binary(distance, M1, M2, P_orb, nburn=1000, nrun=5000, include_M2_photo=True):
+    """
+    Run sampler on one binary
 
-    # Load Gaia observation times
+    Arguments
+    ---------
+    distance : float
+        Distance to the binary
+    M1, M2 : float
+        Component masses (Msun)
+    P_orb : float
+        Orbital period (days)
+    nburn, nrun : int
+        Number of steps in burn-in and run
+    include_M2_photo : bool
+        Add photometric constraints to likelihood
+
+    Returns
+    -------
+    sampler : emcee sampler
+        MCMC emcee sampler
+    """
+
+    # Load Gaia observation times and angles
     data = load_t_obs()
     t_obs = data['ObservationTimeAtBarycentreBarycentricJulianDateInTCB']
     obs_angle = data['scanAnglerad']
@@ -72,19 +154,20 @@ def run_one_binary(distance, M1, M2, P_orb, nburn=1000, nrun=5000, include_M2_ph
                        sys_ra=165.5728333, sys_dec=41.2209444, Omega=np.pi/3.0,
                        omega=np.pi/3.0, I=np.pi/3.0, e=0.01, gamma=0.0)
 
-    # Get observation uncertainty - use Main Sequence star tables
-    # get_V_IC, get_M_G = load_stellar_photometry()
-    # V_IC = get_V_IC(M1/c.Msun)
-    # G_mag = get_M_G(M1/c.Msun) + 5.0*np.log10(distance) - 5.0
+    # Gaia G mmagnitude and error
     G_mag_obs = None
     G_mag_err = None
+
+    # Obtain colors, Gaia G magnitude and error from tables
     V_IC = photometry.get_V_IC(M1/c.Msun)
     G_mag_obs = photometry.get_G_mag_apparent(M1/c.Msun, distance)
     G_mag_err = gaia_photometric.gMagnitudeErrorEoM(G_mag_obs, nobs=len(t_obs))
 
+    # Calculate Gaia along-scan pointing precision
     AL_err = gaia.get_single_obs_pos_err(G=G_mag_obs, V_IC=V_IC, RA=165.5728333, Dec=41.2209444, DIST=distance)
     AL_err *= 1.0e3  # in mas, not asec
 
+    # Include photometric constraints?
     if not include_M2_photo: G_mag_obs = None
 
     # Calculate observed positions
@@ -101,7 +184,35 @@ def run_one_binary(distance, M1, M2, P_orb, nburn=1000, nrun=5000, include_M2_ph
 
 
 def initialize_walkers(p, ra_obs, dec_obs, t_obs, obs_angle, AL_err, G_mag_obs, G_mag_err, nwalkers=128):
+    """
+    Initialize walkers in an n-ball around set of model parameters and emcee sampler
 
+    Arguments
+    ---------
+    p : tuple
+        Set of model parameters
+    ra_obs, dec_obs : numpy array
+        numpy array of positions
+    t_obs : numpy array
+        numpy array of observation times
+    obs_angle : numpy array
+        position angles observed by Gaia
+    AL_err : float
+        Along-scan single-pointing precision
+    G_mag_obs, G_mag_err : float
+        Gaia G magnitude and error
+    nwalkers : int
+        Number of walkers for emcee
+
+    Parameters
+    ----------
+    sampler : emcee sampler
+        Initialized emcee sampler
+    p0 : numpy array
+        Set of numpy arrays
+    """
+
+    # Load model parameters
     sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance, pm_ra, pm_dec = p
 
     # Define sampler
@@ -125,6 +236,7 @@ def initialize_walkers(p, ra_obs, dec_obs, t_obs, obs_angle, AL_err, G_mag_obs, 
     pm_ra_set = pm_ra * (1. + 1.0e-6*np.random.normal(size=nwalkers))
     pm_dec_set = pm_dec * (1. + 1.0e-6*np.random.normal(size=nwalkers))
 
+    # Numpy array of walkers
     p0 = np.array([sys_ra_set,
                    sys_dec_set,
                    Omega_set,
@@ -144,37 +256,80 @@ def initialize_walkers(p, ra_obs, dec_obs, t_obs, obs_angle, AL_err, G_mag_obs, 
 
 
 def get_pos_obs(p, t_obs, obs_angle, AL_err):
+    """
+    Generate mock observations
 
+    Arguments
+    ---------
+    p : tuple
+        Set of model parameters
+    t_obs : numpy array
+        Observation times
+    obs_angles : numpy array
+        Position angles of observations (rad)
+    AL_err : float
+        Along-scan single-pointing precision
+
+    Returns
+    -------
+    ra_obs, dec_obs : float
+        Observed positions (deg)
+    """
+
+    # RA and Dec positions
     ra_obs = np.zeros(len(t_obs))
     dec_obs = np.zeros(len(t_obs))
 
+    # Iterate through observation times
     for i, t in enumerate(t_obs):
+
+        # Calculate the orbital position as a function of model parameters at a time t
         ra_obs_i, dec_obs_i = orbit.get_ra_dec_all(p, t)
         if ra_obs_i is None or dec_obs_i is None: return None, None
 
+        # Create the array of observed positions
         ra_obs[i], dec_obs[i] = ra_obs_i, dec_obs_i
 
-        # Covariance matrix
+        # Create the along-scan vs. across-scan covariance matrix (in deg)
         obs_cov = np.array([[AL_err**2, 0.0],
                             [0.0, gaia.AC_err**2]])
+
+        # Calculate the rotation matrix corresponding to the angle observed by Gaia
         rotation_matrix = np.array([[np.cos(obs_angle[i]), -np.sin(obs_angle[i])],
                                     [np.sin(obs_angle[i]), np.cos(obs_angle[i])]])
+
+        # Obtain the covariance matrix in ra vs. dec space
         new_cov = rotation_matrix @ obs_cov @ rotation_matrix.T
 
-        # Add multivariate errors
+        # Create the position object for the observation
         obs_astrometry = multivariate_normal(mean=np.zeros(2), cov=new_cov)
+
+        # Add multivariate errors
         obs_sample = obs_astrometry.rvs(size=1)
         ra_obs[i] += obs_sample[0]/3600.0/1.0e3
         dec_obs[i] += obs_sample[1]/3600.0/1.0e3
-
-    # Errors are in mas, while ra_tmp is in degrees
-    # ra_obs = ra_tmp + (pos_err/3600.0/1.0e3)*np.random.normal(size=len(t_obs))
-    # dec_obs = dec_tmp + (pos_err/3600.0/1.0e3)*np.random.normal(size=len(t_obs))
 
     return ra_obs, dec_obs
 
 
 def run_emcee(sampler, p0, nburn=100, nrun=1000):
+    """
+    Run the emcee sampler
+
+    Arguments
+    ---------
+    sampler : emcee sampler
+        Initialized emcee sampler object
+    p0 : numpy array
+        Set of initialized walkers
+    nburn, nrun : int
+        Number of burn-in and run steps
+
+    Returns
+    -------
+    sampler : emcee sampler
+        Run emcee sampler object
+    """
 
     pos,prob,state = sampler.run_mcmc(p0, N=nburn)
 
@@ -187,12 +342,32 @@ def run_emcee(sampler, p0, nburn=100, nrun=1000):
 
 
 def run_only_one(dist, M1, M2, P_orb, nburn, nrun, include_M2_photo=False):
+    """
+    Run a single binary
 
+    Arguments
+    ---------
+    dist : float
+        Distance to the binary (pc)
+    M1, M2 : float
+        Component masses (Msun)
+    P_porb : float
+        Orbital period (days)
+    nburn, nrun : int
+        Number of burn-in and run steps
+    include_M2_photo : bool
+        Include photometric constraints
+    """
+
+    # Run a single binary
     sampler = run_one_binary(dist, M1*c.Msun, M2*c.Msun, P_orb*c.secday,
                              nburn=nburn, nrun=nrun, include_M2_photo=include_M2_photo)
 
+    # Create the fileout string
     fileout = "../data/M1_" + str(int(M1)) + '_M2_%.3f'%M2 + '_dist_' + str(int(dist)) + '_Porb_%.3f'%P_orb
-    if include_M2_photo: fileout = fileout + '_photo' 
+    if include_M2_photo: fileout = fileout + '_photo'
+
+    # Save sampler
     np.save(fileout + "_chains.npy", sampler.chain[:,::100,:])
     np.save(fileout + "_acceptancefractions.npy", sampler.acceptance_fraction)
 
@@ -209,172 +384,3 @@ if len(sys.argv) > 5: include_M2_photo=True
 # Run the binary
 # run_only_one(dist, M1, M2, P_orb, 1000, 10000, include_M2_photo=False)
 run_only_one(dist, M1, M2, P_orb, 1000, 10000, include_M2_photo=include_M2_photo)
-
-
-
-
-
-
-
-
-##############
-#   OLD CODE #
-##############
-#
-#
-#
-# def load_data(file_name):
-#
-#     data = np.genfromtxt(file_name)
-#
-#     names = ["time","ra","dec","ra_err","dec_err","rv","rv_err","plx","plx_err"]
-#     obs_pos = np.recarray(len(data[:,0]), names=names,
-#                           formats=['float64,float64,float64,float64,float64,float64,float64,float64,float64'])
-#
-#     obs_pos['time'] = data[:,0]  # in seconds
-#     obs_pos['ra'] = data[:,1]/3600.0/1.0e3  # in degrees
-#     obs_pos['dec'] = data[:,2]/3600.0/1.0e3  # in degrees
-#     obs_pos['rv'] = (2./3.) * data[:,3]/1.0e3  # in km/s
-#
-#     obs_pos['ra_err'] = data[:,4]/1.0e6 # in asec
-#     obs_pos['dec_err'] = data[:,4]/1.0e6 # in asec
-#     obs_pos['rv_err'] = 1.0
-#
-#     return obs_pos
-#
-#
-#
-#
-# def adjust_distance(obs_pos, distance_new, distance_current=2003.0, mag_current=12.5):
-#     """Adjust object according to new distance
-#
-#     """
-#
-#     V_IC = -0.5417
-#
-#     mag_new = gaia.calc_new_magnitude(mag_current, distance_current, distance_new)
-#
-#     plx_err = gaia.get_plx_err(G=mag_new, V_IC=V_IC) * 1.0e3  # plx error in mas
-#
-#     obs_pos['plx'] = 1.0e3 / distance_new
-#     obs_pos['plx_err'] = plx_err
-#
-#
-#     # Adjust position coordinates
-#     obs_pos['ra'] *= distance_current / distance_new
-#     obs_pos['dec'] *= distance_current / distance_new
-#
-#     obs_pos['ra_err'] = gaia.get_single_obs_err(G=mag_new, V_IC=V_IC)
-#     obs_pos['dec_err'] = gaia.get_single_obs_err(G=mag_new, V_IC=V_IC)
-#
-#     return obs_pos
-#
-# def add_uncertainties(obs_pos):
-#     obs_pos['ra'] += np.random.normal(scale=obs_pos['ra_err']/3600.0, size=len(obs_pos))
-#     obs_pos['dec'] += np.random.normal(scale=obs_pos['dec_err']/3600.0, size=len(obs_pos))
-#     obs_pos['rv'] += np.random.normal(scale=obs_pos['rv_err'], size=len(obs_pos))
-#
-#     return obs_pos
-#
-#
-# def get_truths(distance_new):
-#
-#     sys_ra = 1.0e-10
-#     sys_dec = 1.0e-10
-#     Omega = 2.1
-#     omega = 2.5
-#     I = 0.3
-#     # tau = 0.33 + P/2.1
-#     e = 0.7
-#     P = 2.0*3600.0*24.0*365.25
-#     tau = P/500.0
-#     tau = 100.0
-#     gamma = 1.0e-4
-#     M1 = 10.0*c.Msun
-#     M2 = 20.0*c.Msun
-#
-#     truths = sys_ra, sys_dec, Omega, omega, I, tau, e, P, gamma, M1, M2, distance_new
-#
-#     return truths
-#
-#
-# def initialize_sampler(truths, obs_pos, nwalkers=100):
-#
-#     ndim = 12
-#
-#     # Starting array
-#     p0 = np.zeros((nwalkers, ndim))
-#
-#     # Random deviations from best fit
-#     delta = 1.0 + np.random.normal(0.0, 0.001, size=(nwalkers, ndim))
-#
-#     # Adjust starting array accordingly
-#     for i in range(ndim): p0[:,i] = truths[i] * delta[:,i]
-#
-#     return p0
-#
-#
-# def run_emcee(p0, obs_pos):
-#
-#     nwalkers, ndim = p0.shape
-#
-#     # Define sampler
-#     sampler = emcee.EnsembleSampler(nwalkers=nwalkers, dim=ndim, lnpostfn=prob.ln_posterior, args=(obs_pos,))
-#
-#     # Run burn-in
-#     pos,ln_prob,state = sampler.run_mcmc(p0, N=1000)
-#
-#     # Run sampler
-#     sampler.reset()
-#     pos,ln_prob,state = sampler.run_mcmc(pos, N=5000)
-#
-#     # Downsample by factor of 10
-#     chains = sampler.chain[:,::10,:]
-#
-#     return chains
-#
-#
-# def run_one_binary(distance, rv_err):
-#
-#     # Load binary data
-#     obs_pos = load_data("../data/Eccentric_True_RA_Dec_RV.txt")
-#
-#     # Adjust binary to new distance
-#     obs_pos = adjust_distance(obs_pos, distance)
-#
-#     # Adjust binary's RV error
-#     obs_pos['rv_err'] = rv_err
-#
-#     # Add uncertainties to position and RV measurements
-#     obs_pos = add_uncertainties(obs_pos)
-#
-#     # Calculate truths for the binary
-#     truths = get_truths(distance)
-#
-#     # Initialize the emcee sampler
-#     p0 = initialize_sampler(truths, obs_pos, nwalkers=100)
-#
-#     # Run the sampler
-#     chains = run_emcee(p0, obs_pos)
-#
-#     # Save the chains
-#     file_out = "../data/binary_chains/distance" + "%.2f" % distance + "_rverr" + "%.2f" % rv_err + ".npy"
-#     np.save(file_out, chains)
-#
-#
-#
-# def run_grid(n_dist=10, n_rv_err=10, dist_range=(50, 10000), rv_err_range=(0.3, 20.0)):
-#
-#     start_time = time.time()
-#
-#     dist_set = 10**np.linspace(np.log10(dist_range[0]), np.log10(dist_range[1]), n_dist)
-#     rv_err_set = 10**np.linspace(np.log10(rv_err_range[0]), np.log10(rv_err_range[1]), n_rv_err)
-#
-#     for dist in dist_set:
-#         for rv_err in rv_err_set:
-#
-#             print("Running:", dist, rv_err, time.time()-start_time)
-#             run_one_binary(dist, rv_err)
-#
-#
-# run_grid()
